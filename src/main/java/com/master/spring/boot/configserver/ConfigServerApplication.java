d3236d2c709b9494063f154f07a699964eb25ea0
Vik <vikram.projects.33@gmail.com>
2024-03-14 19:42:41 +0530

    Added basic configs

--- src/main/java/com/master/spring/boot/configserver/ConfigServerApplication.java
+++ src/main/java/com/master/spring/boot/configserver/ConfigServerApplication.java
@@ -1,2 +1,13 @@
-package com.master.spring.boot.configserver;public class ConfigServerApplication {
+package com.master.spring.boot.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
---
+++ src/test/java/com/master/spring/boot/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.spring.boot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
